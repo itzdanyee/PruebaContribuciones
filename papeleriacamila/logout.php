4afc30eda2939553a268fce44f239befbaffba0c
Camila Daniela Arzola Mendoza <camilaarzola80@gmail.com>
2024-12-06 21:21:50 -0600

    Delete papeleriacamila directory

--- papeleriacamila/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// Destruir la sesión
-session_unset();
-session_destroy();
-// Redirigir al usuario a la página de inicio de sesión
-header("Location: login.php");
-exit();
-?>
